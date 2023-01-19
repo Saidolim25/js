d9ea181a5675427909c186310894974e77e3c5dd
Saidolim25 <saidasadov2003@gmail.com>
2023-01-20 00:00:09 +0500

    js

---
+++ main.js
@@ -0,0 +1,115 @@
+let score = 3;
+switch (score) {
+    case 1:
+        console.log("Yomon");
+        break;
+    case 2:
+        console.log("Qoniqarsiz");
+        break;
+    case 3:
+        console.log("Qoniqarli");
+        break;
+    case 4:
+        console.log("Yaxshi");    
+        break;
+    case 5 :
+        console.log("A'lo");
+        break;
+        default:
+}
+
+let month = 1;
+switch (month) {
+    case 1:
+        console.log("yanvar");
+        break;
+    case 2:
+        console.log("Fevral");
+        break;
+     case 3:
+         console.log("Mart");    
+        break;
+    case 4:
+        console.log("Aprel");
+       break;
+    case 5:
+        console.log("May");
+        break;
+    case 6 :
+        console.log("Iyun");
+        break;
+    case 7:
+        console.log("Iyul");
+        break;
+    case 8:
+        console.log("Avgust");
+        break;
+    case 9:
+        console.log("Sentyabr");
+        break;
+    case 10:
+        console.log("Oktyabr");
+        break;
+    case 11:
+        console.log("Noyabr");
+        break;
+    case 12:
+        console.log("Dekabr");
+        default:
+}
+
+let month2 = "aprel";
+switch (month2) {
+    case yanvar:
+        console.log(30);
+        break;
+    case fevral:
+        console.log(31);
+        break;
+     case mart:
+         console.log(30);    
+        break;
+    case aprel:
+        console.log(31);
+       break;
+    case may:
+        console.log(30);
+        break;
+    case iyun :
+        console.log(31);
+        break;
+    case iyul:
+        console.log(30);
+        break;
+    case avgust:
+        console.log(31);
+        break;
+    case sentyabr:
+        console.log(30);
+        break;
+    case oktyabr:
+        console.log(31);
+        break;
+    case noyabr:
+        console.log(30);
+        break;
+    case dekabr:
+        console.log(31);
+        default:
+}
+
+let studenScore =5;
+switch (studentScore) {
+    case 5:
+        console.log("3.000.000 so'm");
+        break;
+    case 4:
+        console.log("2.500.000 so'm");
+        break;
+    case 3 :
+        console.log("1.800.000 so'm");
+        break;
+    case 2 :
+        console.log("sizga stipendiya yo'q");
+}
+
